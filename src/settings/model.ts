0d87673ab9647097b7de69f5aaf5e3c241bc4738
Matthew Brooks <matt@msbrooks.com>
2026-07-23 01:55:45 -0500

    restore git and worktrees settings tab

--- src/settings/model.ts
+++ src/settings/model.ts
@@ -122,7 +122,7 @@ export const SETTING_DEFINITIONS: SettingDefinition[] = [
 		requiresRestart: false,
 		sensitive: false,
 		envVar: "CONFIGURED_REPOSITORIES",
-		category: "repositories",
+		category: "server",
 	},
 	{
 		key: "onboarding_complete",
